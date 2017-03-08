d40025a866fb19d07efaea62349e5d69a8dc9c65
boybebe <boybebe@gmail.com>
2017-03-08 13:31:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include 'connect.php';
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>ค้นหาข้อมูล : itoffside.com</title>
+        <title>ระบบค้นหาข้อมูล</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/bootstrap-theme.min.css" rel="stylesheet">
         <style>
